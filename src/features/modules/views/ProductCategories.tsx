2be98dedc3d8c122a5520a5fe7a404540efc9db3
Lefebvre <elefebvre@teksystems.com>
2022-01-31 15:58:32 -0500

    updated pictures

--- src/features/modules/views/ProductCategories.tsx
+++ src/features/modules/views/ProductCategories.tsx
@@ -3,6 +3,12 @@ import ButtonBase from "@mui/material/ButtonBase";
 import Container from "@mui/material/Container";
 import { styled } from "@mui/material/styles";
 import * as React from "react";
+import BodyPicture from "../../assets/body-cream.jpg";
+import CreamPicture from "../../assets/cream.jpg";
+import FacePicture from "../../assets/eyes.jpg";
+import GiftPicture from "../../assets/gift.jpg";
+import HairPicture from "../../assets/hair.jpg";
+import PerfumePicture from "../../assets/perfume.jpg";
 import Typography from "../components/Typography";
 
 const ImageBackdrop = styled("div")(({ theme }) => ({
@@ -55,37 +61,37 @@ const ImageIconButton = styled(ButtonBase)(({ theme }) => ({
 
 const images = [
   {
-    url: "https://images.unsplash.com/photo-1523309996740-d5315f9cc28b?auto=format&fit=crop&w=400&q=80",
+    url: BodyPicture,
     title: "New",
     width: "100%",
   },
   {
-    url: "https://images.unsplash.com/photo-1534081333815-ae5019106622?auto=format&fit=crop&w=400&q=80",
+    url: FacePicture,
     title: "Face",
     width: "50%",
   },
   {
-    url: "https://images.unsplash.com/photo-1531299204812-e6d44d9a185c?auto=format&fit=crop&w=400&q=80",
+    url: HairPicture,
     title: "Hair",
     width: "50%",
   },
   {
-    url: "https://images.unsplash.com/photo-1476480862126-209bfaa8edc8?auto=format&fit=crop&w=400&q=80",
+    url: CreamPicture,
     title: "Body",
     width: "50%",
   },
   {
-    url: "https://images.unsplash.com/photo-1453747063559-36695c8771bd?auto=format&fit=crop&w=400&q=80",
+    url: PerfumePicture,
     title: "Scent",
     width: "50%",
   },
   {
-    url: "https://images.unsplash.com/photo-1534452203293-494d7ddbf7e0?auto=format&fit=crop&w=400&q=80",
+    url: HairPicture,
     title: "Bath & Shower",
     width: "50%",
   },
   {
-    url: "https://images.unsplash.com/photo-1506941433945-99a2aa4bd50a?auto=format&fit=crop&w=400&q=80",
+    url: GiftPicture,
     title: "Gifts",
     width: "50%",
   },
